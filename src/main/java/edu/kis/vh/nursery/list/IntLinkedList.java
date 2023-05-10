c44043c370dc45ce3936628af5873989c6d79041
Mateusz <matiput01@gmail.com>
2023-05-10 11:20:16 +0100

    11. encapsulation - non-private

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -12,9 +12,9 @@ public class IntLinkedList {
         if (last == EMPTY)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -29,14 +29,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return ERROR;
-        return last.value;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
             return ERROR;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 
